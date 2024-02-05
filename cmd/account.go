c8fcaef51dea89c1cc1a9c58eb240366e2b7efbc
PR <creature@localhost.localdomain>
2024-02-05 14:02:21 -0800

    postpone client creation, use reflection to call

--- cmd/account.go
+++ cmd/account.go
@@ -3,10 +3,12 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 	"reflect"
 	"strings"
 
 	"github.com/CreatureDev/xrpl-go/client"
+	jsonrpcclient "github.com/CreatureDev/xrpl-go/client/jsonrpc"
 	"github.com/CreatureDev/xrpl-go/model/client/account"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -34,96 +36,22 @@ func printResponse(res any) {
 	fmt.Println(string(s))
 }
 
-func RunForRequest(req client.XRPLRequest) func(cl *client.XRPLClient, command *cobra.Command, args []string) {
-	method := req.Method()
-	switch method {
-	case "account_channels":
-		return func(cl *client.XRPLClient, command *cobra.Command, args []string) {
-			acr := req.(*account.AccountChannelsRequest)
-			res, _, err := cl.Account.AccountChannels(acr)
-			if err != nil {
-				fmt.Println(err.Error())
-			} else {
-				printResponse(res)
-			}
+func RunForRequest(req client.XRPLRequest, field string, method string) func(*cobra.Command, []string) {
+	return func(*cobra.Command, []string) {
+		cl := NewClient()
+		rf := reflect.ValueOf(cl).Elem()
+		f := rf.FieldByName(field)
+		m := f.MethodByName(method)
+		results := m.Call([]reflect.Value{reflect.ValueOf(req)})
+		if !results[2].IsNil() {
+			fmt.Println(results[2].Elem())
+		} else {
+			printResponse(results[0].Interface())
 		}
-	case "account_currencies":
-		return func(cl *client.XRPLClient, command *cobra.Command, args []string) {
-			acr := req.(*account.AccountCurrenciesRequest)
-			res, _, err := cl.Account.AccountCurrencies(acr)
-			if err != nil {
-				fmt.Println(err.Error())
-			} else {
-				printResponse(res)
-			}
-		}
-	case "account_info":
-		return func(cl *client.XRPLClient, command *cobra.Command, args []string) {
-			air := req.(*account.AccountInfoRequest)
-			res, _, err := cl.Account.AccountInfo(air)
-			if err != nil {
-				fmt.Println(err.Error())
-			} else {
-				printResponse(res)
-			}
-		}
-	case "account_lines":
-		return func(cl *client.XRPLClient, command *cobra.Command, args []string) {
-			alr := req.(*account.AccountLinesRequest)
-			res, _, err := cl.Account.AccountLines(alr)
-			if err != nil {
-				fmt.Println(err.Error())
-			} else {
-				printResponse(res)
-			}
-		}
-	case "account_nfts":
-		return func(cl *client.XRPLClient, command *cobra.Command, args []string) {
-			anr := req.(*account.AccountNFTsRequest)
-			res, _, err := cl.Account.AccountNFTs(anr)
-			if err != nil {
-				fmt.Println(err.Error())
-			} else {
-				printResponse(res)
-			}
-		}
-	case "account_objects":
-		return func(cl *client.XRPLClient, command *cobra.Command, args []string) {
-			aor := req.(*account.AccountObjectsRequest)
-			res, _, err := cl.Account.AccountObjects(aor)
-			if err != nil {
-				fmt.Println(err.Error())
-			} else {
-				printResponse(res)
-			}
-		}
-	case "account_offers":
-		return func(cl *client.XRPLClient, command *cobra.Command, args []string) {
-			aor := req.(*account.AccountOffersRequest)
-			res, _, err := cl.Account.AccountOffers(aor)
-			if err != nil {
-				fmt.Println(err.Error())
-			} else {
-				printResponse(res)
-			}
-		}
-	case "account_tx":
-		return func(cl *client.XRPLClient, command *cobra.Command, args []string) {
-			atr := req.(*account.AccountTransactionsRequest)
-			res, _, err := cl.Account.AccountTransactions(atr)
-			if err != nil {
-				fmt.Println("tx " + err.Error())
-			} else {
-				printResponse(res)
-			}
-		}
-	}
-	return func(*client.XRPLClient, *cobra.Command, []string) {
-		fmt.Println(method + " unimplemented")
 	}
 }
 
-func initRequest(req client.XRPLRequest) {
+func initRequest(req client.XRPLRequest, field string, method string) {
 	var newCmd = &cobra.Command{
 		Use: req.Method(),
 	}
@@ -169,7 +97,7 @@ func initRequest(req client.XRPLRequest) {
 				newCmd.Flags().StringVar(&specifier, tag, "", tag)
 				viper.BindPFlag(tag, newCmd.Flags().Lookup(tag))
 			default:
-				fmt.Println("failed parsing " + tag)
+				fmt.Println(req.Method() + ": failed parsing " + tag)
 			}
 
 		}
@@ -177,19 +105,28 @@ func initRequest(req client.XRPLRequest) {
 			newCmd.MarkFlagRequired(tag)
 		}
 	}
-	newCmd.Run = withClient(RunForRequest(req))
 
+	newCmd.Run = RunForRequest(req, field, method)
+}
+
+func NewClient() *client.XRPLClient {
+	cfg, err := client.NewJsonRpcConfig(Host)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	return jsonrpcclient.NewClient(cfg)
 }
 
 func init() {
 	rootCmd.AddCommand(accountCmd)
 
-	initRequest(&AccountChannels)
-	initRequest(&AccountCurrencies)
-	initRequest(&AccountInfo)
-	initRequest(&AccountLines)
-	initRequest(&AccountNFTs)
-	initRequest(&AccountObjects)
-	initRequest(&AccountOffers)
-	initRequest(&AccountTransactions)
+	initRequest(&AccountChannels, "Account", "AccountChannels")
+	initRequest(&AccountCurrencies, "Account", "AccountCurrencies")
+	initRequest(&AccountInfo, "Account", "AccountInfo")
+	initRequest(&AccountLines, "Account", "AccountLines")
+	initRequest(&AccountNFTs, "Account", "AccountNFTs")
+	initRequest(&AccountObjects, "Account", "AccountObjects")
+	initRequest(&AccountOffers, "Account", "AccountOffers")
+	initRequest(&AccountTransactions, "Account", "AccountTransactions")
 }
